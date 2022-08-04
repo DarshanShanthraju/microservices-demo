2687e90b175658f79636ebc91d5e46712f45056d
DarshanShanthraju <darshan.shanthraju@criticalriver.com>
2022-08-04 13:01:51 +0530

    Updated setup-gke.sh

--- setup-gke.sh
+++ setup-gke.sh
@@ -4,17 +4,52 @@
 # Create a project and set GKE_PROJECT to the project id:
 # See https://console.cloud.google.com/projectselector2/home/dashboard
 
-# Set the PROJECT_ID environment variable and ensure the Google Kubernetes Engine and Cloud Operations APIs are enabled
-PROJECT_ID="cr-gcp-devsecops-353409"
-gcloud services enable container.googleapis.com --project ${PROJECT_ID}
-gcloud services enable monitoring.googleapis.com \
-    cloudtrace.googleapis.com \
-    clouddebugger.googleapis.com \
-    cloudprofiler.googleapis.com \
-    --project ${PROJECT_ID}
+# Set parameters
+export GKE_PROJECT=cr-gcp-devsecops-353409
+export GKE_REGION=europe-west3
+export GKE_ZONE=europe-west3-a
+export GKE_CLUSTER=onlineboutique
+export GKE_SERVICE_ACCOUNT=github-deployment
 
 # Create a GKE cluster
-ZONE=us-central1-b
 gcloud container clusters create onlineboutique \
-    --project=${PROJECT_ID} --zone=${ZONE} \
+    --project=$PROJECT_ID --zone=$GKE_ZONE \
     --machine-type=e2-standard-2 --num-nodes=4
+
+# Configure kubctl
+gcloud container clusters get-credentials $GKE_CLUSTER
+
+# enable API
+gcloud services enable \
+	containerregistry.googleapis.com \
+	container.googleapis.com \
+  artifactregistry.googleapis.com
+
+# Create repository
+gcloud artifacts repositories create $GKE_PROJECT \
+   --repository-format=docker \
+   --location=$GKE_REGION \
+   --description="Docker repository"
+
+# Create a service account
+gcloud iam service-accounts create $GKE_SERVICE_ACCOUNT \
+    --display-name "GitHub Deployment" \
+    --description "Used to deploy from GitHub Actions to GKE"
+
+# Get mail of service account
+gcloud iam service-accounts list
+
+GKE_SVC_MAIL="$GKE_SERVICE_ACCOUNT@$GKE_PROJECT.iam.gserviceaccount.com"
+
+# Add 'container.clusterAdmin' role:
+gcloud projects add-iam-policy-binding $GKE_PROJECT \
+  --member=serviceAccount:$GKE_SVC_MAIL \
+  --role=roles/container.clusterAdmin 
+
+# Add 'artifactregistry.admin' role:
+gcloud projects add-iam-policy-binding $GKE_PROJECT \
+  --member=serviceAccount:$GKE_SVC_MAIL \
+  --role=roles/artifactregistry.admin
+
+# Download JSON
+gcloud iam service-accounts keys create key.json --iam-account=$GKE_SVC_MAIL
